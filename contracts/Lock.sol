385a3fd962a5a21add33d41e853747379cdaa832
Adilkhan Kussidenov <adhan2a@gmail.com>
2023-04-19 19:11:13 +0300

    hwslv

---
+++ contracts/Lock.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is Ownable, ERC20 {
+    constructor() ERC20("Diamond", "DMD") { }
+
+    function mint(address account, uint256 amount) public onlyOwner {
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount) public onlyOwner {
+        _burn(account, amount);
+    }
+}
